8d98117eaf3b063d8d4f953461e4b12ff3230d45
Faiz Zubair <faizvk14@gmail.com>
2026-02-07 14:45:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+// Get input from user
+const num = Number(prompt());
+
+// Calculate factorial
+let factorial = 1;
+for (let i = 1; i <= num; i++) {
+  factorial *= i;
+}
+
+// Display result
+alert(`The factorial of ${num} is ${factorial}`);
